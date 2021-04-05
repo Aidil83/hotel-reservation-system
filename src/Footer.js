09a32692c06c424c89beb60b3a6daba4e283d5fe
Aidil <aidilrozi83@gmail.com>
2021-04-04 22:34:17 -0500

    Installed material ui dependecy and icons.

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,10 @@
+import HomeIcon from "@material-ui/icons/Home";
 const Footer = () => {
-  return <>Footer</>;
+  return (
+    <div>
+      <HomeIcon />
+    </div>
+  );
 };
 
 export default Footer;
