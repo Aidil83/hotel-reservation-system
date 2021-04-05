1439f3579d277be771a0a01f3d41d3e8e0381b63
Aidil <aidilrozi83@gmail.com>
2021-04-04 22:19:06 -0500

    Almost finished with html content. Created a Footer component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Let's Find Your Room</h1>
+      <div className="hero">
+        <div className="hero__image"></div>
+        <div className="hero__title">The Curtain Hotel</div>
+        <div className="hero__ratings">* * * * *</div>
+        <div className="hero__footer">
+          <div className="pricing">$100</div>
+          <div className="book-room">BOOK ROOM</div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
