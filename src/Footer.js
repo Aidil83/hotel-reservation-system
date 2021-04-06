1f2e6748b5b9bf5833ae9d40a2f01338563c200c
Aidil <aidilrozi83@gmail.com>
2021-04-05 23:35:27 -0500

    Imported all icons for Footer component.

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,22 @@
-import { Icon, InlineIcon } from "@iconify/react";
+import { Icon } from "@iconify/react";
 import bxHome from "@iconify/icons-bx/bx-home";
-import HomeIcon from "@material-ui/icons/Home";
+import twotonePayments from "@iconify/icons-ic/twotone-payments";
+import mail24Regular from "@iconify/icons-fluent/mail-24-regular";
+import profileIcon from "@iconify/icons-gg/profile";
+
 const Footer = () => {
   return (
     <div>
       <Icon icon={bxHome} style={{ color: "#add8e6", fontSize: "38px" }} />
+      <Icon
+        icon={twotonePayments}
+        style={{ color: "#add8e6", fontSize: "46px" }}
+      />
+      <Icon
+        icon={mail24Regular}
+        style={{ color: "#add8e6", fontSize: "41px" }}
+      />
+      <Icon icon={profileIcon} style={{ color: "#add8e6", fontSize: "35px" }} />
     </div>
   );
 };
