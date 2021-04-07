3467fd6927a1a679b5841f9d6c1672ba83ee6ee3
Aidil <aidilrozi83@gmail.com>
2021-04-07 10:26:22 -0500

    imported curtain hotel image and styled card.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
 import Footer from "./Footer";
+import curtain_hotel from "./images/curtain_hotel.jpg";
 function App() {
   return (
     <div className="App">
       <h1>Let's Find Your Rooms</h1>
       <div className="hero">
-        <div className="hero__image"></div>
-        <div className="hero__title">The Curtain Hotel</div>
-        <div className="hero__ratings">* * * * *</div>
-        <div className="hero__footer">
-          <div className="pricing">$100</div>
-          <div className="book-room">BOOK ROOM</div>
+        <div className="hero__card">
+          <img src={curtain_hotel} className="hero__image" />
+          <div className="hero__info">
+            <div className="hero__title">The Curtain Hotel</div>
+            <div className="hero__ratings">* * * * *</div>
+            <div className="hero__footer">
+              <div className="pricing">$100</div>
+              <div className="book-room">BOOK ROOM</div>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
