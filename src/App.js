fedbc1557a79d077d88c311201864b09bd1bdba8
Aidil <aidilrozi83@gmail.com>
2021-04-06 20:22:48 -0500

    Finshed with app title.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 function App() {
   return (
     <div className="App">
-      <h1>Let's Find Your Room</h1>
+      <h1>Let's Find Your Rooms</h1>
       <div className="hero">
         <div className="hero__image"></div>
         <div className="hero__title">The Curtain Hotel</div>
