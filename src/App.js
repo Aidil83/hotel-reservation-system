21406786d564dd3791b56469d7a9a187f91128d4
Aidil <aidilrozi83@gmail.com>
2021-04-08 23:40:57 -0500

    Created Card component.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import "./App.css";
+import Card from "./Card";
 import Footer from "./Footer";
-import curtain_hotel from "./images/curtain_hotel.jpg";
-import star_ratings from "./images/star_ratings.png";
 function App() {
   return (
     <div className="App">
       <h1>Let's Find Your Rooms</h1>
       <div className="hero">
-        <div className="hero__card">
-          <img src={curtain_hotel} className="hero__image" />
-          <div className="hero__info">
-            <div className="hero__title">The Curtain Hotel</div>
-            <img src={star_ratings} className="hero__ratings" />
-            <div className="hero__footer">
-              <div className="pricing">$100</div>
-              <div className="book-room">BOOK ROOM</div>
-            </div>
-          </div>
-        </div>
+        <Card />
       </div>
       <Footer />
     </div>
---
+++ src/Card.js
@@ -0,0 +1,20 @@
+import curtain_hotel from "./images/curtain_hotel.jpg";
+import star_ratings from "./images/star_ratings.png";
+
+const Card = () => {
+  return (
+    <div className="hero__card">
+      <img src={curtain_hotel} className="hero__image" />
+      <div className="hero__info">
+        <div className="hero__title">The Curtain Hotel</div>
+        <img src={star_ratings} className="hero__ratings" />
+        <div className="hero__footer">
+          <div className="pricing">$100</div>
+          <div className="book-room">BOOK ROOM</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
