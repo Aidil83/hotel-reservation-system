03aa6be7dc1e068e92f1c21d5c62cced136478f8
Aidil <aidilrozi83@gmail.com>
2021-04-11 19:41:52 -0500

    Gave different image for each hero card.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import "./App.css";
 import Hero from "./Hero";
 import Footer from "./Footer";
+import curtain_hotel from "./images/curtain_hotel.jpg";
+import albert_vincent_hotel from "./images/albert_vincent_hotel.jpg";
+import dana_hotel from "./images/dana_hotel.jpg";
+import olexandr_hotel from "./images/olexandr_hotel.jpg";
+import venice_hotel from "./images/venice_hotel.jpg";
+
 function App() {
   return (
     <div className="App">
       <h1>Let's Find Your Rooms</h1>
       <div className="hero">
-        <Hero />
-        <Hero />
-        <Hero />
-        <Hero />
+        <Hero image={curtain_hotel} />
+        <Hero image={albert_vincent_hotel} />
+        <Hero image={dana_hotel} />
+        <Hero image={olexandr_hotel} />
+        <Hero image={venice_hotel} />
       </div>
       <Footer />
     </div>
--- src/Hero.js
+++ src/Hero.js
@@ -1,11 +1,10 @@
 import "./Hero.css";
-import curtain_hotel from "./images/curtain_hotel.jpg";
 import star_ratings from "./images/star_ratings.png";
 
-const Hero = () => {
+const Hero = ({ image }) => {
   return (
     <div className="hero__card">
-      <img src={curtain_hotel} className="hero__image" />
+      <img src={image} className="hero__image" />
       <div className="hero__info">
         <div className="hero__title">The Curtain Hotel</div>
         <img src={star_ratings} className="hero__ratings" />
