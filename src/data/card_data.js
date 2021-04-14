6b640e6d5f31b4cea04e64325fd8a65ac5c97a2a
Aidil <aidilrozi83@gmail.com>
2021-04-14 10:02:45 -0500

    Created card_data that contains all card info.

---
+++ src/data/card_data.js
@@ -0,0 +1,27 @@
+export const card_data = [
+  {
+    title: "The Curtain Room",
+    rating: 5,
+    price: 600,
+  },
+  {
+    title: "Albert Vincent Room",
+    rating: 4,
+    price: 200,
+  },
+  {
+    title: "Dana Room",
+    rating: 3,
+    price: 700,
+  },
+  {
+    title: "Olexandr Room",
+    rating: 2,
+    price: 100,
+  },
+  {
+    title: "Venice Room",
+    rating: 5,
+    price: 900,
+  },
+];
