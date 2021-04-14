ca58e496e8ca4b2a5b3c309df583d1a4282d44bf
Aidil <aidilrozi83@gmail.com>
2021-04-14 10:09:13 -0500

    Imported and pass by prop card_data.

--- src/App.js
+++ src/App.js
@@ -7,17 +7,18 @@ import dana_hotel from "./images/dana_hotel.jpg";
 import olexandr_hotel from "./images/olexandr_hotel.jpg";
 import venice_hotel from "./images/venice_hotel.jpg";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { card_data } from "./data/card_data";
 
 function App() {
   return (
     <div className="App">
       <h1>Let's Find Your Rooms</h1>
       <div className="hero">
-        <Hero image={curtain_hotel} />
-        <Hero image={albert_vincent_hotel} />
-        <Hero image={dana_hotel} />
-        <Hero image={olexandr_hotel} />
-        <Hero image={venice_hotel} />
+        <Hero image={curtain_hotel} card_data={card_data[0]} />
+        <Hero image={albert_vincent_hotel} card_data={card_data[1]} />
+        <Hero image={dana_hotel} card_data={card_data[2]} />
+        <Hero image={olexandr_hotel} card_data={card_data[3]} />
+        <Hero image={venice_hotel} card_data={card_data[4]} />
       </div>
       <Footer />
     </div>
--- src/Hero.js
+++ src/Hero.js
@@ -1,15 +1,16 @@
 import "./Hero.css";
 import star_ratings from "./images/star_ratings.png";
 
-const Hero = ({ image }) => {
+const Hero = ({ image, card_data }) => {
+  const { title, rating, price } = card_data;
   return (
     <div className="hero__card">
       <img src={image} className="hero__image" />
       <div className="hero__info">
-        <div className="hero__title">The Curtain Hotel</div>
+        <div className="hero__title">{title}</div>
         <img src={star_ratings} className="hero__ratings" />
         <div className="hero__footer">
-          <div className="pricing">$100</div>
+          <div className="pricing">{price}</div>
           <div className="book-room">BOOK ROOM</div>
         </div>
       </div>
